14957670b8193c578208d961ae5259c65e00d8a3
valisaros <valeriu.saros@gmail.com>
2020-08-28 14:39:22 +0400

    Changed the logo font

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
     
     burger.addEventListener('click', () => {
+        
         //Toggle Nav
         nav.classList.toggle('nav-active');
+        
 
         //Animate Links
         navLinks.forEach((link, index) => {
@@ -17,12 +18,10 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
-
+        
         
         //Burger animation
         burger.classList.toggle('toggle');
-
-
     });
 }
 
