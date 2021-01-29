1d976c4e62172f559695a37102d75d5277708ad6
Valeriu Saros <valeriu.saros@gmail.com>
2021-01-29 12:23:00 +0200

    Fixed burger menu

--- js/main.js
+++ js/main.js
@@ -3,14 +3,18 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
     const body = document.body;
+    const link1 = document.querySelector('.link1');
+    const link2 = document.querySelector('.link2');
+    const link3 = document.querySelector('.link3');
 
     burger.addEventListener('click', () => {
-        
+
         //Toggle Nav
         nav.classList.toggle('nav-active');
         // Set the body position to fixed when burger clicked
         //body.style.position = 'fixed';
-        
+        burger.style.position = 'fixed';
+
         //Animate Links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
@@ -20,17 +24,77 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
-        
-        
+
+
         //Burger animation
         burger.classList.toggle('toggle');
-        
+
+
         // Disable the fixed position when burger clicked
-        //navLinks.addEventListener('click', () => {
-            //body.style.position = '';
-        //})
-        
+        // burger.style.position = '';
     });
+
+    link1.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        burger.style.position = '';
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+    link2.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        burger.style.position = '';
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+
+    link3.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        burger.style.position = '';
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
 }
 
 navSlide();
